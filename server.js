8e28b450883e2ad19a8b528281e405536e26137b
Daniel Kindt <danieltkindt@gmail.com>
2017-11-03 10:47:45 -0600

    added mongo stuff

--- server.js
+++ server.js
@@ -4,20 +4,32 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
 const app = express();
+var db;
+
+app.set('view engine', 'ejs');
+
+MongoClient.connect("mongodb://localhost/", (err, database) => {
+  if (err) return console.warn(err)
+  db = database
+});
 
 app.listen(3000, function() {
     console.log('listening on 3000')
-})
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+    db.collection('quotes').find().toArray((err, result) => {
+      if (err) return console.warn(err)
+      res.render('index.ejs', {quotes: result})
+    })
+});
 
 app.post('/quotes', (req, res) => {
-    console.log('req.body')
-})
-
-MongoClient.connect("mongodb://localhost/", (err, db) => {
-  if (err) return console.warn(err)
-  var myDB = db
+    db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.warn(err)
+      console.log('saved to database')
+      res.redirect('/');
+    })
 });
+
+
